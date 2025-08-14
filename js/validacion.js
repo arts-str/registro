4892dfef2107f321b1a725e753b88fb35cdf5bb5
ARTS-str <magninfacundo@gmail.com>
2025-08-14 13:39:34 -0300

    Tarea 7-3-1

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,35 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const email = document.getElementById('email');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const terminos = document.getElementById('terminos');
+
+function camposLlenos() {
+    return nombre.value != '' && apellido.value != '' && email.value != '' && password1.value != '' && password2.value != '';
+}
+
+function largoContraseña() {
+    return password1.value.length >= 6;
+}
+
+function sonIguales() {
+    return password1.value === password2.value;
+}
+
+function checkboxMarcado() {
+    return terminos.checked;
+}
+
+function validacion() {
+    console.log(camposLlenos() && largoContraseña() && sonIguales() && checkboxMarcado());
+    if (camposLlenos() && largoContraseña() && sonIguales() && checkboxMarcado()) {
+        showAlertSuccess();
+    }else{
+        showAlertError();
+    }
+}
